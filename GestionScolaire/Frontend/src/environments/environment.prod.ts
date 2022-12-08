6b39ebaba1d1765bc06ea55839d563fd014cb82c
fanta.diakite <dfanta155@gmail.com>
2022-12-08 10:09:07 +0000

    update

--- GestionScolaire/Frontend/src/environments/environment.prod.ts
+++ GestionScolaire/Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
-export const environment = {
-  production: true
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+ export const ENVIRONMENT = {
+  API:"/api",
+
+  production: true,
 };
+  
